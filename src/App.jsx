df593addd37d7e8ec0e03f80e9c8dc3bfd582634
Oliver Stevens <oliverwstevens@gmail.com>
2025-09-27 18:42:05 -0400

    Made todo form

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 import "./CheckBox.css";
-import { create,readFile, mkdir, BaseDirectory } from '@tauri-apps/plugin-fs';
+import { create, readFile, mkdir, BaseDirectory } from '@tauri-apps/plugin-fs';
 import { appLocalDataDir } from "@tauri-apps/api/path";
 
 export default class App extends Component {
@@ -116,9 +116,9 @@ export default class App extends Component {
       <main className="app">
         <header className="content-header">
           <div className="content-header__details">
-            
+
             <h1 className="content-header__title">Workplace</h1>
-            
+
           </div>
           <div className="content-header__aside">
             <button className="btn btn--primary btn--icon">
@@ -131,28 +131,33 @@ export default class App extends Component {
           <div className="todo-panel__content">
 
             <div className="todo">
-            <div className="checkbox-wrapper">
-              <input className="inp-cbx" id="cbx" type="checkbox" style={{ display: 'none' }} />
-              <label className="cbx" htmlFor="cbx">
-                <span>
-                  <svg width="12px" height="9px" viewBox="0 0 12 9">
-                    <polyline points="1 5 4 8 11 1" />
-                  </svg>
-                </span>
-                <span>To-do aoiwj w waiofj ewj waeifj owfjew iowae jfio</span>
-              </label>
-            </div>
+              <div className="checkbox-wrapper">
+                <input className="inp-cbx" id="cbx" type="checkbox" style={{ display: 'none' }} />
+                <label className="cbx" htmlFor="cbx">
+                  <span>
+                    <svg width="12px" height="9px" viewBox="0 0 12 9">
+                      <polyline points="1 5 4 8 11 1" />
+                    </svg>
+                  </span>
+                  <span>To-do aoiwj w waiofj ewj waeifj owfjew iowae jfio</span>
+                </label>
+              </div>
               <button className="btn btn--primary btn--icon">
                 <span className="material-symbols-outlined icon--high-emphasis icon icon--x-large">domain</span>
               </button>
             </div>
           </div>
 
-         
+
         </div>
 
         <div className="todo-form">
-          bye
+          <div className="todo">
+            <input placeholder="Add todo..." autoFocus/>
+            <button className="btn btn--primary btn--icon">
+              <span className="material-symbols-outlined icon--high-emphasis icon icon--x-large">domain</span>
+            </button>
+          </div>
         </div>
 
 
